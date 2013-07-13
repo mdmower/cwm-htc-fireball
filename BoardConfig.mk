6457469bbe442525368db9f8d96458ded190e9f9
Matt Mower <mowerm@gmail.com>
2013-07-13 12:39:47 -0500

    Formatting update

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -3,26 +3,36 @@ USE_CAMERA_STUB := true
 # inherit from the proprietary version
 -include vendor/htc/fireball/BoardConfigVendor.mk
 
-TARGET_ARCH := arm
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := fireball
 TARGET_NO_BOOTLOADER := true
+
+# Platform
 TARGET_BOARD_PLATFORM := msm8960
 TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
+
+# Architecture
+TARGET_ARCH := arm
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_SMP := true
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_ARCH_VARIANT_CPU := cortex-a9
 ARCH_ARM_HAVE_TLS_REGISTER := true
-TARGET_CPU_SMP := true
 TARGET_USE_KRAIT_BIONIC_OPTIMIZATION := true
 
-TARGET_BOOTLOADER_BOARD_NAME := fireball
-
+# Kernel
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=qcom
 BOARD_KERNEL_BASE := 0x80400000
 BOARD_KERNEL_PAGESIZE := 2048
+TARGET_PREBUILT_KERNEL := device/htc/fireball/kernel
+
+# for jellybean branch of CM use BOARD_FORCE_RAMDISK_ADDRESS
+# for cm-10.1 branch of CM use BOARD_MKBOOTIMG_ARGS
+# BOARD_FORCE_RAMDISK_ADDRESS := 0x81800000
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01400000
 
-# fix this up by examining /proc/emmc on a running device
+# Filesystem (cat /proc/emmc)
 TARGET_USERIMAGES_USE_EXT4 := true
 BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16776704
@@ -30,9 +40,9 @@ BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1207958528
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 1241513472
 BOARD_FLASH_BLOCK_SIZE := 131072
 
-TARGET_PREBUILT_KERNEL := device/htc/fireball/kernel
-
+# Use power button as select in recovery
 BOARD_HAS_NO_SELECT_BUTTON := true
 
+# Vold
 BOARD_VOLD_MAX_PARTITIONS := 36
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/file
--- Readme.md
+++ Readme.md
@@ -1,5 +1,11 @@
-ClockworkMod - HTC Droid Incredible 4G LTE
-------------------------------------------
-This is a recovery-only device tree to build ClockworkMod for the HTC Droid Incredible 4G LTE (htc fireball). This source can also be used to build [CWMR-Touch by gweedo767](https://github.com/mdmower/android_bootable_recovery/commit/fa2cc0023b0a07699b5a670c79b32208b845c35b).  
+**ClockworkMod - HTC Droid Incredible 4G LTE**
+
+This is a recovery-only device tree for the HTC Droid Incredible 4G LTE (htc fireball).
+
+**Quick build instructions:**
+
+    . build/envsetup.sh
+    lunch cm_fireball-eng
+    mka recoveryimage
 
 If you are interested in building CyanogenMod, use the official device tree at [GitHub/Cyanogenmod/android\_device\_htc\_fireball](https://github.com/cyanogenmod/android_device_htc_fireball).
